7d0b7e5f605e5bfc4db80e659964b7d42ecec168
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-18 23:24:46 +0100

    styling header

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   state = {};
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
         <h1> Mani Tube</h1>
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div>Searchbar</div>;
+    return (
+      <div className=" search-bar ui segment violet inverted header">
+        <form className="ui form ">
+          <div className="field">
+            <label className="ui grey inverted header">Search Video</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
