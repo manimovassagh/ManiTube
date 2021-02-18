4c3425621ac655f8825a81b61b801ed459f4d48a
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-18 21:19:25 +0100

    Searchbar hardcoded

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <h1> Mani Tube</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
