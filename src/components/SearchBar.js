298b73a9dda3c857a6dc20ed86fba6f928b93ef1
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-18 23:40:03 +0100

    adding prevent default and form submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    // make sure to call
+    //callback from parent component
+  };
   render() {
     return (
       <div className=" search-bar ui segment violet inverted header">
-        <form className="ui form ">
+        <form onSubmit={this.onFormSubmit} className="ui form ">
           <div className="field">
             <label className="ui grey inverted header">Search Video</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
